e210fecff8d23cadeb45efa32bea99e4ec10dbfe
Vipavee <vi_vpk_4ever@hotmail.com>
2017-08-15 14:41:21 +0700

    Design and show Data

---
+++ app/src/main/java/com/example/vipavee/learning_database/TodoListActivity.java
@@ -0,0 +1,54 @@
+package com.example.vipavee.learning_database;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class TodoListActivity extends AppCompatActivity {
+
+    String[] itemname = {
+            "Get theatre tickets",
+            "Order pizza for tonight",
+            "Buy groceries",
+            "Play Video Game at 19.30",
+            "Call to Note"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ListView lv = (ListView) findViewById(R.id.lvTodos);
+//adds the custom layout
+        lv.setAdapter(new ArrayAdapter<String>(this, R.layout.todo_list_item,
+                R.id.tvNote, itemname));
+//adds the click event to the listView, reading the content
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+                Intent intent = new Intent(TodoListActivity.this, TodoActivity.class);
+                String content = (String) lv.getItemAtPosition(pos);
+                intent.putExtra("Content", content);
+                startActivity(intent);
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
